35e485c9c8de579fefdf72845499c462e46e5ac4
swayam <swayamwr@gmail.com>
2026-07-10 07:54:03 +0530

    trigger backend CI/CD pipeline

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,6 +13,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
+        "https://unbuilt-ten.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
